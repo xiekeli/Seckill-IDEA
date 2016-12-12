71026e0566e1050f5b1c617b26a1809deeb389c6
Cciradih <565114731@qq.com>
2016-12-12 20:20:55 +0800

    Initial commit

---
+++ src/main/java/com/seckill/web/SeckillController.java
@@ -0,0 +1,44 @@
+package com.seckill.web;
+
+import com.seckill.entity.Seckill;
+import com.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/*url:/模块/资源/{id}/细分*/
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private SeckillService seckillService;
+
+    /*获取列表*/
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+        List<Seckill> seckillList = seckillService.getSeckillList();
+        model.addAttribute("seckillList", seckillList);
+        return "list";
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail", method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model) {
+        if (seckillId == null) {
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = seckillService.getSeckillById(seckillId);
+        if (seckill == null) {
+            return "forward:/seckill/list";
+        }
+        model.addAttribute("secill", seckill);
+        return "detail";
+    }
+}
